07e44b6633d4f7afc97a51a5c6db7067f879a809
Isabella Skořepová <isabella@skorepova.info>
2023-11-23 00:13:40 +0100

    add ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,11 +1,14 @@
 let
+  # cat ~/.ssh/id_ed25519.pub on IsblAsahi
+  asahi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZdRoS3HXiUh77MLq2OczaysE79CK0NZGfHyH+3tBlv";
+
   # cat ~/.ssh/id_ed25519.pub on IsblDesktop
-  isabella = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFrYVxQiKKIzGqLIO+6w6qA1d+E9vR2bFLW0EuT4e6zA";
-  users = [ isabella ];
+  desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFrYVxQiKKIzGqLIO+6w6qA1d+E9vR2bFLW0EuT4e6zA";
+  users = [ desktop asahi ];
 
   # cat /etc/ssh/ssh_host_ed25519_key.pub on data.isbl.cz
   data = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDg+C4BYTdAx2BMQgj/5uT64WkJc7o6L7emjjK6UYJ/m";
-  systems = [ data ];
+  systems = [ data ];  
 in
 {
   #"secret1.age".publicKeys = [ user1 system1 ];
