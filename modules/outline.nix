a824bca26e2ad3b19d182ac735a1df6f420c0f79
Isabella Skořepová <isabella@skorepova.info>
2023-11-29 22:27:38 +0100

    use authentik for outline

--- modules/outline.nix
+++ modules/outline.nix
@@ -15,42 +15,15 @@
     enableUpdateCheck = false;
     oidcAuthentication = {
       # Parts taken from
-      # https://dex.isbl.cz/.well-known/openid-configuration
-      authUrl = "https://dex.isbl.cz/auth";
-      tokenUrl = "https://dex.isbl.cz/token";
-      userinfoUrl = "https://dex.isbl.cz/userinfo";
+      # https://authentik.isbl.cz/application/o/outline/.well-known/openid-configuration
+      authUrl = "https://authentik.isbl.cz/application/o/authorize/";
+      tokenUrl = "https://authentik.isbl.cz/application/o/token/";
+      userinfoUrl = "https://authentik.isbl.cz/application/o/userinfo/";
       clientId = "outline";
       clientSecretFile = config.age.secrets.outline.path;
       scopes = [ "openid" "email" "profile" ];
       usernameClaim = "preferred_username";
-      displayName = "Dex";
-    };
-  };
-  services.dex = {
-    enable = true;
-    settings = {
-      issuer = "https://dex.isbl.cz";
-      storage.type = "sqlite3";
-      web.http = "127.0.0.1:5556";
-      staticClients = [
-        {
-          id = "outline";
-          name = "Outline Client";
-          redirectURIs = [ "https://outline.isbl.cz/auth/oidc.callback" ];
-          secretFile = config.age.secrets.outline.path;
-        }
-      ];
-      connectors = [
-        {
-          type = "mockPassword";
-          id = "isabella";
-          name = "Isabella Skořepová";
-          config = {
-            username = "isbl";
-            password = "password";
-          };
-        }
-      ]; 
+      displayName = "Authentik";
     };
   };
 }
