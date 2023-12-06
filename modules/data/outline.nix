82a61423f679cb686aea58541577ca3fcc876795
Isabella Skořepová <isabella@skorepova.info>
2023-12-06 02:30:19 +0100

    data is in postgres 14

--- modules/data/outline.nix
+++ modules/data/outline.nix
@@ -9,6 +9,7 @@
     file = ../../secrets/outline.age;
     owner = "outline";
   };
+  services.postgresql.package = pkgs.postgresql_14;
   services.outline = {
     enable = true;
     publicUrl = "https://outline.isbl.cz";
