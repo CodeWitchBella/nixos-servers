f0f234a53cc1c94233f84224161744d0f0fc31ae
Isabella Skořepová <isabella@skorepova.info>
2024-04-06 01:15:15 +0200

    disable arrs

--- fake-modules/data/servarr.nix
+++ fake-modules/data/servarr.nix
@@ -2,25 +2,26 @@
   pkgs,
   config,
   inputs,
+  lib,
   ...
-}: {
+}: rec {
   services.lidarr = {
-    enable = true;
+    enable = false;
     dataDir = "/ssd/lidarr";
   };
   services.radarr = {
-    enable = true;
+    enable = false;
     dataDir = "/ssd/radarr";
   };
   services.readarr = {
-    enable = true;
+    enable = false;
     dataDir = "/ssd/readarr";
   };
   services.sonarr = {
-    enable = true;
+    enable = false;
     dataDir = "/ssd/sonarr";
   };
-  services.prowlarr.enable = true;
+  services.prowlarr.enable = false;
 
   users.groups.jellyfin = {
     name = "jellyfin";
@@ -28,14 +29,22 @@
   };
 
   users.users = {
-    lidarr.extraGroups = ["jellyfin"];
-    lidarr.uid = 306;
-    radarr.extraGroups = ["jellyfin"];
-    radarr.uid = 275;
-    readarr.extraGroups = ["jellyfin"];
-    readarr.uid = 993;
-    sonarr.extraGroups = ["jellyfin"];
-    sonarr.uid = 274;
+    lidarr = lib.mkIf services.lidarr.enable {
+      extraGroups = ["jellyfin"];
+      lidarr.uid = 306;
+    };
+    radarr = lib.mkIf services.radarr.enable {
+      extraGroups = ["jellyfin"];
+      uid = 275;
+    };
+    readarr = lib.mkIf services.readarr.enable {
+      extraGroups = ["jellyfin"];
+      uid = 993;
+    };
+    sonarr = lib.mkIf services.sonarr.enable {
+      extraGroups = ["jellyfin"];
+      uid = 274;
+    };
 
     jellyfin.name = "jellyfin";
     jellyfin.uid = 997;
