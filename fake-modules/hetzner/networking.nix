d1b9b94a1a8e7f7a587d11b36919038c5342b13a
Isabella Skořepová <isabella@skorepova.info>
2024-08-17 21:43:24 +0200

    simplify and fix networking

--- fake-modules/hetzner/networking.nix
+++ fake-modules/hetzner/networking.nix
@@ -3,37 +3,24 @@
   config,
   inputs,
   ...
-}: let
-  external-mac = "90:1b:0e:de:e6:56";
-  ext-if = "et0";
-  external-ip = "176.9.41.240";
-  external-gw = "176.9.41.255";
-  external-ip6 = "2a01:4f8:161:3797::1";
-  external-gw6 = "fe80::1";
-  external-netmask = 27;
-  external-netmask6 = 64;
-in {
-  # rename the external interface based on the MAC of the interface
-  services.udev.extraRules = ''SUBSYSTEM=="net", ATTR{address}=="${external-mac}", NAME="${ext-if}"'';
-  networking = {
-    interfaces."${ext-if}" = {
-      ipv4.addresses = [
-        {
-          address = external-ip;
-          prefixLength = external-netmask;
-        }
-      ];
-      ipv6.addresses = [
-        {
-          address = external-ip6;
-          prefixLength = external-netmask6;
-        }
-      ];
-    };
-    defaultGateway6 = {
-      address = external-gw6;
-      interface = ext-if;
-    };
-    defaultGateway = external-gw;
+}: {
+  networking.useDHCP = false;
+  networking.interfaces."enp0s31f6".ipv4.addresses = [
+    {
+      address = "176.9.41.240";
+      prefixLength = 24;
+    }
+  ];
+  networking.interfaces."enp0s31f6".ipv6.addresses = [
+    {
+      address = "2a01:4f8:161:3797::1";
+      prefixLength = 64;
+    }
+  ];
+  networking.defaultGateway = "176.9.41.225";
+  networking.defaultGateway6 = {
+    address = "fe80::1";
+    interface = "enp0s31f6";
   };
+  networking.nameservers = ["8.8.8.8"];
 }
