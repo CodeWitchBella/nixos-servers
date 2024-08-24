8467f764419a01cad42194207cfd1d8f61fcd2ee
Isabella Skořepová <isabella@skorepova.info>
2024-08-25 01:40:48 +0200

    simple minecraft setup

--- fake-modules/hetzner/hetzner.nix
+++ fake-modules/hetzner/hetzner.nix
@@ -7,6 +7,7 @@
   imports = [
     ./disk-config.nix
     ./impermanence.nix
+    ./minecraft.nix
     ./networking.nix
     ./restic.nix
   ];
@@ -22,4 +23,11 @@
     enable = true;
     databases = ["test"];
   };
+
+  networking.firewall.allowedTCPPorts = [
+    22 # ssh
+    80 # http
+    25565 # minecraft
+    #7000 # frps
+  ];
 }
